b6ce1f37dd349bdde181ada4894f4fc8a580f8bd
Tim La Roche <timroche@expediagroup.com>
2021-12-13 13:15:14 +0000

    finish day 9

--- day9/pt2/main.go
+++ day9/pt2/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -20,16 +21,16 @@ func main() {
 	grid := [102][102]int{}
 
 	// Pad grid
-	for j := 0; j < 102; j++ {
+	for j := 0; j < 12; j++ {
 		grid[0][j] = 9999
 	}
-	for j := 0; j < 102; j++ {
+	for j := 0; j < 12; j++ {
 		grid[101][j] = 9999
 	}
-	for i := 0; i < 102; i++ {
+	for i := 0; i < 7; i++ {
 		grid[i][0] = 9999
 	}
-	for i := 0; i < 102; i++ {
+	for i := 0; i < 7; i++ {
 		grid[i][101] = 9999
 	}
 
@@ -45,6 +46,17 @@ func main() {
 		i++
 	}
 
+	fmt.Println(grid)
+
+	// TEST
+	somePoints := make([]Point, 2)
+	somePoints[0] = *&Point{1, 1, 500}
+	somePoints[1] = *&Point{1, 1, 500}
+	fmt.Println(somePoints)
+	somePoints = removeDuplicates(somePoints)
+	fmt.Println(somePoints)
+	//
+
 	// get basin points
 	basins := []Point{}
 	sum := 0
@@ -52,46 +64,85 @@ func main() {
 		for j := 1; j < 101; j++ {
 			if checkAllAdjacent(i, j, grid, true) {
 				basins = append(basins, *&Point{i, j, grid[i][j]})
-				sum += grid[i][j] + 1
+				sum += (grid[i][j] + 1)
 			}
 		}
 	}
 	basinScore := make([]int, len(basins))
 	fmt.Println(sum)
-
+	fmt.Println(basins)
+	fmt.Println(grid[1][1])
+	fmt.Println("====")
 	// first loop
 	for i, point := range basins {
+		visited := map[string]bool{}
 		queue := []Point{}
 		basinScore[i] += 1
-		if checkAllAdjacent(point.x, point.y, grid, true) {
-			lp, _ := checkLeft(point.x, point.y, grid)
-			rp, _ := checkRight(point.x, point.y, grid)
-			up, _ := checkUp(point.x, point.y, grid)
-			dp, _ := checkDown(point.x, point.y, grid)
-			queue = append(queue, lp, rp, up, dp)
-		}
-		fmt.Println(queue)
+		lp, _ := checkLeft(point.x, point.y, grid)
+		rp, _ := checkRight(point.x, point.y, grid)
+		up, _ := checkUp(point.x, point.y, grid)
+		dp, _ := checkDown(point.x, point.y, grid)
+		fmt.Printf("Checking point: x:%d, y:%d, val:%d\n", point.x, point.y, point.val)
+		fmt.Printf("u: %d,d: %d,l: %d,r: %d\n", up.val, dp.val, lp.val, rp.val)
+		queue = append(queue, lp, rp, up, dp)
 		queue = removeNines(queue)
-		fmt.Println(queue)
+		queue = removeDuplicates(queue)
 		//now loop onwards until empty
 		for len(queue) != 0 {
+			basinScore[i] += 1
 			fmt.Println(queue)
-			for _, point := range queue {
-				queue = queue[1:]
-				basinScore[i] += 1
-				if checkAllAdjacent(point.x, point.y, grid, false) {
-					lp, _ := checkLeft(point.x, point.y, grid)
-					rp, _ := checkRight(point.x, point.y, grid)
-					up, _ := checkUp(point.x, point.y, grid)
-					dp, _ := checkDown(point.x, point.y, grid)
-					queue = append(queue, lp, rp, up, dp)
-				}
-				queue = removeNines(queue)
+			queuePoint := queue[0]
+			visited[fmt.Sprintf("%d%d", queuePoint.x, queuePoint.y)] = true
+			queue = queue[1:]
+			fmt.Printf("Checking point: x:%d, y:%d, val:%d\n", queuePoint.x, queuePoint.y, queuePoint.val)
+			lp, _ := checkLeft(queuePoint.x, queuePoint.y, grid)
+			rp, _ := checkRight(queuePoint.x, queuePoint.y, grid)
+			up, _ := checkUp(queuePoint.x, queuePoint.y, grid)
+			dp, _ := checkDown(queuePoint.x, queuePoint.y, grid)
+			fmt.Printf("u: %d,d: %d,l: %d,r: %d\n", up.val, dp.val, lp.val, rp.val)
+			// dont go backwards
+			switch queuePoint.val {
+			case lp.val:
+				fmt.Println("dont go back left")
+				lp.val = 99998
+			case rp.val:
+				fmt.Println("dont go back right")
+				rp.val = 99998
+			case up.val:
+				fmt.Println("dont go back up")
+				up.val = 99998
+			case dp.val:
+				fmt.Println("dont go back down")
+				dp.val = 99998
 			}
+			if !visited[fmt.Sprintf("%d%d", lp.x, lp.y)] {
+				queue = append(queue, lp)
+
+			}
+			if !visited[fmt.Sprintf("%d%d", rp.x, rp.y)] {
+				queue = append(queue, rp)
+
+			}
+			if !visited[fmt.Sprintf("%d%d", dp.x, dp.y)] {
+				queue = append(queue, dp)
+
+			}
+			if !visited[fmt.Sprintf("%d%d", up.x, up.y)] {
+				queue = append(queue, up)
+			}
+			queue = removeNines(queue)
+			queue = removeDuplicates(queue)
+			fmt.Printf("Updated queue: %v\n", queue)
 		}
+		fmt.Println(basinScore[i])
+		fmt.Println("===newbasin===")
 	}
 
+	sort.Ints(basinScore)
 	fmt.Println(basinScore)
+	size := len(basinScore)
+	finalScore := basinScore[size-1] * basinScore[size-2] * basinScore[size-3]
+	fmt.Println(finalScore)
 }
 
 func removeNines(b []Point) []Point {
@@ -101,6 +152,20 @@ func removeNines(b []Point) []Point {
 			newB = append(newB, point)
 		}
 	}
+	return newB
+}
+
+func removeDuplicates(b []Point) []Point {
+	newB := []Point{}
+	exists := make(map[string]bool)
+	for _, point := range b {
+		key := fmt.Sprintf("%d%d", point.x, point.y)
+		if _, exist := exists[key]; !exist {
+			exists[key] = true
+			newB = append(newB, point)
+		}
+	}
+	//fmt.Println(exists)
 	return newB
 }
 
@@ -121,7 +186,7 @@ func checkAllAdjacent(i int, j int, grid [102][102]int, desc bool) bool {
 	l := grid[i][j-1]
 	r := grid[i][j+1]
 	current := grid[i][j]
-
+	fmt.Printf("u:%d d:%d l:%d r:%d current:%d \n", u, d, l, r, current)
 	if desc {
 		if current < u && current < d && current < l && current < r {
 			return true
